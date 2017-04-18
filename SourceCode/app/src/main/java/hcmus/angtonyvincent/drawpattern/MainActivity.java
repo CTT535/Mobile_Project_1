b2a89fc50608c910faf03e818313621fc82a6590
DESKTOP-EDSKDFB\Ang Tony Vincent <angtonyvincent@yahoo.com.vn>
2017-04-18 23:48:41 +0700

    Update draw pattern

--- SourceCode/app/src/main/java/hcmus/angtonyvincent/drawpattern/MainActivity.java
+++ SourceCode/app/src/main/java/hcmus/angtonyvincent/drawpattern/MainActivity.java
@@ -3,35 +3,80 @@ package hcmus.angtonyvincent.drawpattern;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private String CorrectPattern = "123";
+
+    Button refresh;
+    TextView instruction;
+    private String correctPattern;
     private MaterialLockView materialLockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        materialLockView = (MaterialLockView) findViewById(R.id.pattern);
 
+        setContentView(R.layout.activity_main);
+
+        instruction = (TextView) findViewById(R.id.textViewInstruction);
+
+        refresh = (Button) findViewById(R.id.buttonRefresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newPattern();
+            }
+        });
+
+        newPattern();
+
+        materialLockView = (MaterialLockView) findViewById(R.id.pattern);
         materialLockView.setOnPatternListener(new MaterialLockView.OnPatternListener() {
             @Override
             public void onPatternDetected(List<MaterialLockView.Cell> pattern, String SimplePattern) {
                 Log.e("SimplePattern", SimplePattern);
-                if (!SimplePattern.equals(CorrectPattern)) {
+                if (SimplePattern.equals(correctPattern)) {
 
-                    materialLockView.setDisplayMode(MaterialLockView.DisplayMode.Wrong);
-                    // materialLockView.clearPattern();
+                    materialLockView.setDisplayMode(MaterialLockView.DisplayMode.Correct);
+
+                    newPattern();
 
                 } else {
 
-                    materialLockView.setDisplayMode(MaterialLockView.DisplayMode.Correct);
+                    materialLockView.setDisplayMode(MaterialLockView.DisplayMode.Wrong);
 
                 }
                 super.onPatternDetected(pattern, SimplePattern);
             }
         });
+    }
+
+    public void newPattern() {
+        correctPattern = randomPattern();
+        instruction.setText(correctPattern);
+    }
+
+    public String randomPattern() {
+        String result = "";
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 1; i < 10; i++) {
+            list.add(new Integer(i));
+        }
+
+        Collections.shuffle(list);
+
+        for (int i = 0; i < 3; i++) {
+            result += list.get(i).toString();
+        }
+
+        return result;
     }
 }
